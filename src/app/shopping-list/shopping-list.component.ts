eb73e9f2657ff5f119aeaaa5070e4a8adbeb5c12
Ed Chen <aedyucheng@gmail.com>
2020-10-25 15:23:35 +0800

    add ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients = [
-    // new Ingredient('Apple', 5),
-    // new Ingredient('Tomato', 3)
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomato', 3)
   ];
 
   constructor() { }
